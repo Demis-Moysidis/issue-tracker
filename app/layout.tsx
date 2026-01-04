bcb63f814c3dddf6e557634a1592614e40f4ac99
Demis Moysidis <demis.moysidis@outlook.de>
2026-01-04 02:29:44 +0100

    Build the new issue page

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Theme>
           <NavBar></NavBar>
-          <main>
+          <main className='p-5'>
             {children}
           </main>
         </Theme>
