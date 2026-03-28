fa0e45a3e59a172bebc3119aeb1f90dfb05a341d
Demis Moysidis <demis.moysidis@outlook.de>
2026-03-28 14:38:20 +0100

    Build the LatestIssue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
-export default async function Home({ searchParams }: { searchParams: { page: string }}) {
-  const searchParamsAwait = await searchParams;
+export default async function Home() {
   
   return (
-    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParamsAwait.page)}/>
+    <LatestIssues />
   )
 }
